08ed03046ea1ee6b0483fb59eed990064a534b01
devanshu6445 <44567896+devanshu6445@users.noreply.github.com>
2022-02-05 19:57:34 +0530

    Update Client.java

--- app/src/main/java/com/india/chat/samwaad/Notifications/Client.java
+++ app/src/main/java/com/india/chat/samwaad/Notifications/Client.java
@@ -8,6 +8,7 @@ public class Client {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(String url){
+        //implementing signleton design pattern
         if (retrofit == null){
             retrofit = new Retrofit.Builder()
                     .baseUrl(url)
